7ee3fcf8fee4da164d82ed3d424b825b567afc02
Nicolas Shreiner <DrkMatr1984@aol.com>
2021-10-31 15:53:23 -0400

    Update to 1.17.1

--- src/main/java/com/shepherdjerred/sthorses/util/ItemUtils.java
+++ src/main/java/com/shepherdjerred/sthorses/util/ItemUtils.java
@@ -1,52 +1,89 @@
 package com.shepherdjerred.sthorses.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ItemUtils {
-
-    public static void addGlow(ItemStack item) {
-        ItemMeta meta = item.getItemMeta();
-        meta.addEnchant(Enchantment.DURABILITY, 1, true);
-        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-        item.setItemMeta(meta);
-    }
-
-    public static String getCarpetColorAsString(short color) {
-        List<String> colorNames = new ArrayList<>();
-        colorNames.add(0, "WHITE");
-        colorNames.add(1, "ORANGE");
-        colorNames.add(2, "MAGENTA");
-        colorNames.add(3, "LIGHT_BLUE");
-        colorNames.add(4, "YELLOW");
-        colorNames.add(5, "LIME");
-        colorNames.add(6, "PINK");
-        colorNames.add(7, "GRAY");
-        colorNames.add(8, "SILVER");
-        colorNames.add(9, "CYAN");
-        colorNames.add(10, "PURPLE");
-        colorNames.add(11, "BLUE");
-        colorNames.add(12, "BROWN");
-        colorNames.add(13, "GREEN");
-        colorNames.add(14, "RED");
-        colorNames.add(15, "BLACK");
-        
-        return colorNames.get(color);
-    }
-
-    public static short getCarpetColorAsShort(String color) {
-        for (short i = 0; i < 16; i++) {
-            if (getCarpetColorAsString(i).equalsIgnoreCase(color)) {
-                return i;
-            }
-        }
-
-        return 0;
-    }
-
+  public static void addGlow(ItemStack item) {
+    ItemMeta meta = item.getItemMeta();
+    meta.addEnchant(Enchantment.DURABILITY, 1, true);
+    meta.addItemFlags(new ItemFlag[] { ItemFlag.HIDE_ENCHANTS });
+    item.setItemMeta(meta);
+  }
+  
+  public static String getCarpetColorAsString(short color) {
+    List<String> colorNames = new ArrayList<>();
+    colorNames.add(0, "WHITE");
+    colorNames.add(1, "ORANGE");
+    colorNames.add(2, "MAGENTA");
+    colorNames.add(3, "LIGHT_BLUE");
+    colorNames.add(4, "YELLOW");
+    colorNames.add(5, "LIME");
+    colorNames.add(6, "PINK");
+    colorNames.add(7, "GRAY");
+    colorNames.add(8, "SILVER");
+    colorNames.add(9, "CYAN");
+    colorNames.add(10, "PURPLE");
+    colorNames.add(11, "BLUE");
+    colorNames.add(12, "BROWN");
+    colorNames.add(13, "GREEN");
+    colorNames.add(14, "RED");
+    colorNames.add(15, "BLACK");
+    return colorNames.get(color);
+  }
+  
+  public static short getCarpetColorAsShort(String color) {
+    for (short i = 0; i < 16; i = (short)(i + 1)) {
+      if (getCarpetColorAsString(i).equalsIgnoreCase(color))
+        return i; 
+    } 
+    return 0;
+  }
+  
+  public static ItemStack getCarpetByColor(String color) {
+    switch (color) {
+      case "WHITE":
+        return new ItemStack(Material.WHITE_CARPET, 1);
+      case "ORANGE":
+        return new ItemStack(Material.ORANGE_CARPET, 1);
+      case "MAGENTA":
+        return new ItemStack(Material.MAGENTA_CARPET, 1);
+      case "LIGHT_BLUE":
+        return new ItemStack(Material.LIGHT_BLUE_CARPET, 1);
+      case "YELLOW":
+        return new ItemStack(Material.YELLOW_CARPET, 1);
+      case "LIME":
+        return new ItemStack(Material.LIME_CARPET, 1);
+      case "PINK":
+        return new ItemStack(Material.PINK_CARPET, 1);
+      case "GRAY":
+        return new ItemStack(Material.GRAY_CARPET, 1);
+      case "SILVER":
+        return new ItemStack(Material.LIGHT_GRAY_CARPET, 1);
+      case "LIGHT_GRAY":
+        return new ItemStack(Material.LIGHT_GRAY_CARPET, 1);
+      case "CYAN":
+        return new ItemStack(Material.CYAN_CARPET, 1);
+      case "PURPLE":
+        return new ItemStack(Material.PURPLE_CARPET, 1);
+      case "BLUE":
+        return new ItemStack(Material.BLUE_CARPET, 1);
+      case "BROWN":
+        return new ItemStack(Material.BROWN_CARPET, 1);
+      case "GREEN":
+        return new ItemStack(Material.GREEN_CARPET, 1);
+      case "RED":
+        return new ItemStack(Material.RED_CARPET, 1);
+      case "BLACK":
+        return new ItemStack(Material.BLACK_CARPET, 1);
+      case "MOSS":
+        return new ItemStack(Material.MOSS_CARPET, 1);
+    } 
+    return null;
+  }
 }
--- src/main/java/com/shepherdjerred/sthorses/util/PlaceUtils.java
+++ src/main/java/com/shepherdjerred/sthorses/util/PlaceUtils.java
@@ -1,48 +1,59 @@
 package com.shepherdjerred.sthorses.util;
 
+import java.util.List;
 import org.bukkit.Material;
 import org.bukkit.entity.AbstractHorse;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Llama;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.List;
-
 public class PlaceUtils {
-    public static String translateOldVariants(String variant) {
-        switch (variant) {
-        case "DONKEY":
-            return "Horse";
-        case "HORSE":
-            return "Horse";
-        case "MULE":
-            return "Mule";
-        case "SKELETON_HORSE":
-            return "SkeletonHorse";
-        default:
-            return variant;
-        }
-    }
-
-    public static void createHorse(Horse horse, List<String> lore) {
-        String color = lore.stream().filter(str -> str.contains("Color: ")).findFirst().get().replace("Color: ", "");
-        String style = lore.stream().filter(str -> str.contains("Style: ")).findFirst().get().replace("Style: ", "");
-        horse.setColor(Horse.Color.valueOf(color));
-        horse.setStyle(Horse.Style.valueOf(style));
-    }
-
-    public static void createLlama(Llama llama, List<String> lore) {
-        String color = lore.stream().filter(str -> str.contains("Color: ")).findFirst().get().replace("Color: ", "");
-        llama.setColor(Llama.Color.valueOf(color));
-    }
-
-    public static void giveSaddle (AbstractHorse abstractHorse) {
-        ((Horse) abstractHorse).getInventory().setSaddle(new ItemStack(Material.SADDLE, 1));
-    }
-
-    public static void giveCarpet (AbstractHorse abstractHorse, String carpetColor) {
-        ItemStack carpet = new ItemStack(Material.CARPET, 1);
-        carpet.setDurability(ItemUtils.getCarpetColorAsShort(carpetColor));
-        abstractHorse.getInventory().setItem(1, carpet);
-    }
+  public static String translateOldVariants(String variant) {
+    switch (variant) {
+      case "DONKEY":
+        return "Horse";
+      case "HORSE":
+        return "Horse";
+      case "MULE":
+        return "Mule";
+      case "SKELETON_HORSE":
+        return "SkeletonHorse";
+    } 
+    return variant;
+  }
+  
+  public static void createHorse(Horse horse, List<String> lore) {
+    String color = ((String)lore.stream().filter(str -> str.contains("Color: ")).findFirst().get()).replace("Color: ", "");
+    String style = ((String)lore.stream().filter(str -> str.contains("Style: ")).findFirst().get()).replace("Style: ", "");
+    horse.setColor(Horse.Color.valueOf(color));
+    horse.setStyle(Horse.Style.valueOf(style));
+  }
+  
+  public static void createLlama(Llama llama, List<String> lore) {
+    String color = ((String)lore.stream().filter(str -> str.contains("Color: ")).findFirst().get()).replace("Color: ", "");
+    llama.setColor(Llama.Color.valueOf(color));
+  }
+  
+  public static void giveSaddle(AbstractHorse abstractHorse) {
+    ((Horse)abstractHorse).getInventory().setSaddle(new ItemStack(Material.SADDLE, 1));
+  }
+  
+  public static void giveCarpet(AbstractHorse abstractHorse, String carpetColor) {
+    ItemStack carpet = ItemUtils.getCarpetByColor(carpetColor);
+    abstractHorse.getInventory().setItem(1, carpet);
+  }
+  
+  public static boolean isCarpet(ItemStack item) {
+    if (item.getType() == Material.BLACK_CARPET || item.getType() == Material.BLUE_CARPET || item.getType() == Material.BROWN_CARPET || item
+      .getType() == Material.CYAN_CARPET || item.getType() == Material.GRAY_CARPET || item
+      .getType() == Material.GREEN_CARPET || item.getType() == Material.LEGACY_CARPET || item
+      .getType() == Material.LIGHT_BLUE_CARPET || item.getType() == Material.LIGHT_GRAY_CARPET || item
+      .getType() == Material.LIME_CARPET || item.getType() == Material.MAGENTA_CARPET || item
+      .getType() == Material.MOSS_CARPET || item.getType() == Material.ORANGE_CARPET || item
+      .getType() == Material.PINK_CARPET || item.getType() == Material.PURPLE_CARPET || item
+      .getType() == Material.RED_CARPET || item.getType() == Material.WHITE_CARPET || item
+      .getType() == Material.YELLOW_CARPET)
+      return true; 
+    return false;
+  }
 }
